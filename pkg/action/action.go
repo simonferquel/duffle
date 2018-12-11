1c3686001837bb9683bbb8981bf2920f3b393d9e
Simon Ferquel <simon.ferquel@docker.com>
2018-12-11 13:21:09 +0100

    Inject image-map at runtime
    This is the second part of the move to the image map injection move.
    Now that we have a map from logical-name to an image definition
    (containing the image reference, digest, description etc.), we inject it
    in the invocation image in the same way we deal with parameters and
    credentials.
    
    The invocation image will then be able to consume this map to replace
    any utility image reference declared in the bundle.
    
    Signed-off-by: Simon Ferquel <simon.ferquel@docker.com>

--- pkg/action/action.go
+++ pkg/action/action.go
@@ -1,6 +1,7 @@
 package action
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
@@ -39,6 +40,14 @@ func selectInvocationImage(d driver.Driver, c *claim.Claim) (bundle.InvocationIm
 	return bundle.InvocationImage{}, errors.New("driver is not compatible with any of the invocation images in the bundle")
 }
 
+func getImageMap(b *bundle.Bundle) ([]byte, error) {
+	imgs := b.Images
+	if imgs == nil {
+		imgs = make(map[string]bundle.Image)
+	}
+	return json.Marshal(imgs)
+}
+
 func opFromClaim(action string, c *claim.Claim, ii bundle.InvocationImage, creds credentials.Set, w io.Writer) (*driver.Operation, error) {
 	env, files, err := creds.Expand(c.Bundle)
 
@@ -67,6 +76,12 @@ func opFromClaim(action string, c *claim.Claim, ii bundle.InvocationImage, creds
 			env[param.Destination.EnvironmentVariable] = value
 		}
 	}
+
+	imgMap, err := getImageMap(c.Bundle)
+	if err != nil {
+		return nil, fmt.Errorf("unable to generate image map: %s", err)
+	}
+	files["/cnab/app/image-map.json"] = string(imgMap)
 
 	env["CNAB_INSTALLATION_NAME"] = c.Name
 	env["CNAB_ACTION"] = action
